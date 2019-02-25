0e0c1abf44cd236a052d878b078a52911101c856
Nicolas GABRION <nico.gabrion@gmail.com>
2019-02-25 15:26:16 +0100

    -

--- nems/src/index.js
+++ nems/src/index.js
@@ -6,7 +6,4 @@ const connection = require('./db').Connection();
 const connectionInfo = config.get('db');
 
 connection.open(connectionInfo).catch((err) => { console.log(err); });
-let exec = require('child_process').exec;
-let command = 'mongoimport --db api --collection questions --file questionsPrenomSport.json --jsonArray';
-exec(command);
 server.start();
