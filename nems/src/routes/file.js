f92f067ba773e5d933829a6d9f36ed6a372a774f
Nicolas GABRION <nico.gabrion@gmail.com>
2019-02-26 10:52:03 +0100

    -

--- nems/src/routes/file.js
+++ nems/src/routes/file.js
@@ -1,25 +1,23 @@
-var express = require('express');
-var router = express.Router();
-var mongoose = require('mongoose');
-var Question = require('../models/Question');
+const express = require('express');
+const router = express.Router();
+const Question = require('../models/Question');
+const fs = require('fs');
 
 const multer = require('multer');
 
-const upload = multer({ dest: 'uploads/' });
+const upload = multer({ dest: __dirname + '../uploads' });
 
-router.post('/', upload.single('file'), function (req, res, next) {
-    console.log(req.file);
-    var json = require('../../src/questionsPrenomSport.json');
-    console.log(json);
-    Question.insertMany(json, function(err,result) {
-        console.log(json);
-        if (err) {
-          return err;
-        } else {
-          return result;
-        }
-     });
-    return res;
-  });
+router.post('/', upload.single('file'), (req, res, next) => {
+    const json = require('../../uploads/questionsPrenomSport.json');
+    Question.deleteMany({}, async (err, del) => {
+        Question.insertMany(json, async (err, ins) => {
+            Question.find((err, found) => {
+                console.log('del', del);
+                console.log('found', found);
+                res.send('done');
+            });
+        });
+    });
+});
 
 module.exports = router;
