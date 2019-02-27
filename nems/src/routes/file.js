8aaa6905872a21b6632be3d5249ed6f2536b09ab
Nicolas GABRION <nico.gabrion@gmail.com>
2019-02-27 11:34:02 +0100

    -

--- nems/src/routes/file.js
+++ nems/src/routes/file.js
@@ -2,6 +2,22 @@ const express = require('express');
 const router = express.Router();
 const Question = require('../models/Question');
 
+var fs = require('fs');
+
+var multer  = require('multer')
+var upload = multer({ dest: 'uploads/' })
+ 
+router.post('/newfile', upload.single('file'), function (req, res, next) {
+    var obj = JSON.parse(fs.readFileSync(req.file.path, 'utf8'));
+    Question.deleteMany({}, async (err, del) => {
+        Question.insertMany(obj, async (err, ins) => {
+            Question.find((err, found) => {
+                res.send(obj);
+            });
+        });
+    });
+})
+
 router.post('/', (req, res) => {
     const json = require('../../uploads/questionsPrenomSport.json');
     Question.deleteMany({}, async (err, del) => {
