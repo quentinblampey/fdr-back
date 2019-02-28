28097c9dfa71a99ed91743c73514016da1c6ba9a
MariusHull <marius.hullin@gmail.com>
2019-02-28 09:55:42 +0100

    Modified user.js

--- nems/src/models/User.js
+++ nems/src/models/User.js
@@ -1,14 +1,26 @@
-var mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-var UserSchema = new mongoose.Schema({
-  registration : Date,
+const UserSchema = new mongoose.Schema({
+  registration: Date,
   pseudo: String,
-  currentBreak : [Number],
-  nextBreak : [Number],
-  details: {name:String, sportBeforeComing:String,sportNow:String,clubFound:String,sportNowIsFun:String},
-  numberChats : [String],
-  numberQuestions : Number,
-  score: {motivation: {type: Number, default: -1}, fidelity: {type: Number, default: -1}, lifestyle: {type: Number, default: -1}, integration: {type: Number, default: -1}, noOrientation: {type: Number, default: -1}}
+  currentBreak: [Number],
+  nextBreak: [Number],
+  details: {
+    name: String,
+    sportBeforeComing: String,
+    sportNow: String,
+    clubFound: String,
+    sportNowIsFun: String
+  },
+  numberChats: [String],
+  numberQuestions: Number,
+  score: {
+    motivation: { type: Number, default: -1 },
+    fidelity: { type: Number, default: -1 },
+    lifestyle: { type: Number, default: -1 },
+    integration: { type: Number, default: -1 },
+    noOrientation: { type: Number, default: -1 }
+  }
 });
 
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model("User", UserSchema);
