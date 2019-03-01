91dd823c00cdfcf4bfdb75c85f0974734b50294b
Quentin Blampey <quentin.blampey@gmail.com>
2019-03-01 13:42:53 +0100

    new constructs

--- nems/src/models/Question.construct.js
+++ nems/src/models/Question.construct.js
@@ -1,37 +1,78 @@
-var construct = function(question,details) {
-    switch (question.idQ) {
-      case 2:
-        question.body = question.persoBody[0] + " " + details.name + " " + question.persoBody[1];
-        break;
-      case 7:
-        question.body = question.persoBody[0] + " " + details.sportBeforeComing + " " + question.persoBody[1];
-        break;
-      case 10:
-        if (details.sportNow=="same") {
-          question.body = question.persoBody[0] + " " + details.sportBeforeComing + " " + question.persoBody[1];
-        }
-        else {
-          question.body = question.persoBody[0] + " " + details.sportNow + " " + question.persoBody[1];
-        }
-        break;
-      case 11:
-        if (details.sportNow=="same") {
-          question.body = question.persoBody[0] + " " + details.sportBeforeComing + " " + question.persoBody[1];
-        }
-        else {
-          question.body = question.persoBody[0] + " " + details.sportNow + " " + question.persoBody[1];
-        }
-        break;
-      case 12:
-        if (details.sportNow=="same") {
-          question.body = question.persoBody[0] + " " + details.sportBeforeComing + " " + question.persoBody[1];
-        }
-        else {
-          question.body = question.persoBody[0] + " " + details.sportNow + " " + question.persoBody[1];
-        }
-        break;
-    }
-    return question;
-  };
+var construct = function(question, details) {
+  switch (question.idQ) {
+    case 2:
+      question.body =
+        question.persoBody[0] +
+        " " +
+        details.name +
+        " " +
+        question.persoBody[1];
+      break;
+    case 7:
+      question.body = details.sportBeforeComing + " " + question.persoBody[0];
+      break;
+    case 10:
+      if (details.sportNow == "same") {
+        question.body =
+          question.persoBody[0] +
+          " " +
+          details.sportBeforeComing +
+          " " +
+          question.persoBody[1];
+      } else {
+        question.body =
+          question.persoBody[0] +
+          " " +
+          details.sportNow +
+          " " +
+          question.persoBody[1];
+      }
+      break;
+    case 12:
+      if (details.sportNow == "same") {
+        question.body =
+          question.persoBody[0] +
+          " " +
+          details.sportBeforeComing +
+          " " +
+          question.persoBody[1];
+      } else {
+        question.body =
+          question.persoBody[0] +
+          " " +
+          details.sportNow +
+          " " +
+          question.persoBody[1];
+      }
+      break;
+    case 24:
+      if (details.newFriend == "0") {
+        question.body = question.persoBody[0] + " 0 " + question.persoBody[1];
+      } else {
+        question.body =
+          question.persoBody[0] + " moins de 2 " + question.persoBody[1];
+      }
+      break;
+    case 61:
+      if (details.presenceTD == "2") {
+        question.body =
+          question.persoBody[0] + " souvent " + question.persoBody[1];
+      } else {
+        question.body =
+          question.persoBody[0] + " toujours " + question.persoBody[1];
+      }
+      break;
+    case 62:
+      if (details.objectifPresenceTD == "2") {
+        question.body =
+          question.persoBody[0] + " souvent " + question.persoBody[1];
+      } else {
+        question.body =
+          question.persoBody[0] + " toujours " + question.persoBody[1];
+      }
+      break;
+  }
+  return question;
+};
 
 module.exports = construct;
