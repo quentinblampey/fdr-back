643e81db18623e11d834c522439264f89596cd98
Quentin Blampey <quentin.blampey@gmail.com>
2019-03-06 10:56:47 +0100

    to build

--- nems/src/app.js
+++ nems/src/app.js
@@ -20,8 +20,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(function(req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "http://fdr.cs-campus.fr");
   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", "http://fdr.cs-campus.fr");
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
