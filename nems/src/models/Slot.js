d837bc3643a65bc8c01c99a816d95bd2bae52f03
MariusHull <marius.hullin@gmail.com>
2019-03-27 11:30:19 +0100

    Commentaires et clean Models + App.js

--- nems/src/models/Slot.js
+++ nems/src/models/Slot.js
@@ -1,10 +1,14 @@
 var mongoose = require("mongoose");
 
+/*
+Schema of the rendez-vous taken by the student.
+It is not linked to any specific teacher yet.
+*/
 var SlotSchema = new mongoose.Schema({
-  idU: String,
-  date: String,
-  duration: Number,
-  affectation: { type: String, default: "" }
+  idU: String, // The id of the user in case of a RDV directly proposed by the teacher
+  date: String, // The date of the RDV proposed.
+  duration: Number, // The duration of the proposed RDV, in minutes.
+  affectation: { type: String, default: "" } // The id of the user FINALLY affected to the RDV. Empty before the assignment process.
 });
 
 module.exports = mongoose.model("Slot", SlotSchema);
--- nems/src/routes/slots.js
+++ nems/src/routes/slots.js
@@ -1,12 +1,3 @@
-/* eslint-disable curly */
-/* eslint-disable object-shorthand */
-/* eslint-disable comma-dangle */
-/* eslint-disable indent */
-/* eslint-disable space-before-function-paren */
-/* eslint-disable semi */
-/* eslint-disable quotes */
-/* eslint-disable prefer-arrow-callback */
-
 const express = require("express");
 const router = express.Router();
 const Slot = require("../models/Slot.js");
